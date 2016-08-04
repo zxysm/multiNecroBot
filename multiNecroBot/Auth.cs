562b5dc190624176b10c96a7663a6fddbee76d01
numbe9988 <matteobit@hotmail.com>
2016-08-04 13:58:49 +0200

    first relase

---
+++ multiNecroBot/Auth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace multiNecroBot
+{
+    class Auth
+    {
+        public string authType { get; set; }
+        public string googleUsername { get; set; }
+        public string googlePassword { get; set; }
+        public string ptcPassword { get; set; }
+        public string ptcUsername { get; set; }
+    }
+}
